b6064267ae6d97b6274f7ba60a73cb812e3efaae
Edwin Candinegara <edwin.candinegara@gmail.com>
2016-09-13 17:01:23 +0800

    Init

---
+++ data_exploration.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# destinations = pd.read_csv('data/destinations.csv')
+# test = pd.read_csv('data/test.csv')
+train = pd.read_csv('data/train.csv')
+
+# print(destinations.shape)
+# print(test.shape)
+print(train.shape)
+
+print()
+print()
+
+# print(destinations.head(10))
+# print(test.head(10))
+print(train.head(10))
