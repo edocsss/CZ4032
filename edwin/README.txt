84760d72512dcbfca482691bc8e67bc69da96966
Edwin Candinegara <edwin.candinegara@gmail.com>
2016-11-05 02:51:42 +0800

    .

---
+++ edwin/README.txt
@@ -0,0 +1,26 @@
+1. Open <path_to_project_dir>/edwin/util/data_util.py
+2. Change the data set that you want to use for training
+    - The name of the data set depends on the settings that you use when running <path_to_project_dir>/data_split_util.py
+    - By default, there should be set A, B, AB (combined data from A and B), and C
+    - This can be changed under these 2 variables (both values should refer to the dictionary key used in <path_to_project_dir>/data_split_util.py:
+        - X_SET_TO_BE_PREPROCESSED
+        - Y_SET_TO_BE_PREPROCESSED
+
+3. To train the model based on the preprocessed training data:
+    - Linear Regression by Artist: python3 <path_to_project_dir>/edwin/lr_by_artist/train_lr_by_artist.py
+    - Random Forest by Artist: python3 <path_to_project_dir>/edwin/rf_by_artist/train_rf_by_artist.py
+    - Random Forest Full Data Features: python3 <path_to_project_dir>/edwin/rf_full/train_rf_full.py
+
+4. To predict the rating a dataset (A, B, AB, or C):
+    - Change the dataset which you want to predict the ratings for
+        - In the "if __name__ == '__main__'" part, there should be this line of code:
+            - predictions = predict_ratings_and_calculate_mse(<X_SET>, <Y_SET>)
+            - Change the value of <X_SET> and <Y_SET> accordingly
+            - Available value: X_A, Y_A, X_B, Y_B, X_C, Y_C
+
+    - Linear Regression by Artist: python3 <path_to_project_dir>/edwin/lr_by_artist/predict_lr_by_artist.py
+    - Random Forest by Artist: python3 <path_to_project_dir>/edwin/rf_by_artist/predict_rf_by_artist.py
+    - Random Forest Full Data Features: python3 <path_to_project_dir>/edwin/rf_full/predict_rf_full.py
+
+5. The MSE and/or RMSE results should be displayed in the console
+6. There should be some zipped pickle files that contains the predicted ratings which will be used by the ensemble models
---
+++ ensemble/averaging.py
@@ -0,0 +1,130 @@
+import gzip
+import numpy as np
+import pprint
+from sklearn.metrics import mean_squared_error
+import math
+
+from model.helper import *
+
+# Load the results of linear regression by artist models
+def lr_by_artist(suffix='C2'):
+    filename='../edwin/lr_by_artist/lr_by_artist_training_predictions_result_{}.zip'.format(suffix)
+    y=np.array(pickle.load(gzip.GzipFile(filename)))
+    return y
+
+
+# Load the results of Random Forest by artist models
+def rf_by_artist(suffix='C2'):
+    filename='../edwin/rf_by_artist/rf_by_artist_training_predictions_result_{}.zip'.format(suffix)
+    y=np.array(pickle.load(gzip.GzipFile(filename)))
+    return y
+
+
+# Load the results of Random Forest using the full data features
+def rf_full(suffix='C2'):
+    filename='../edwin/rf_full/rf_full_training_predictions_result_{}.zip'.format(suffix)
+    y=np.array(pickle.load(gzip.GzipFile(filename)))
+    return y
+
+
+# Load the results of Gradient Boosting Regression using the full data features
+def gbr(suffix='C2'):
+    filename='../kenrick/gbr_preds_{}.pkl'.format(suffix)
+    with open(filename, 'rb') as f:
+        y=np.array(pickle.load(f))
+    return y
+
+
+# Load the results of the individual Neural Network using the full data features
+def nn(suffix='C2'):
+    filename='../model/nn_y_{}_hat.pkl'.format(suffix)
+    with open(filename, 'rb') as f:
+        y=pickle.load(f)
+    y=y[:,] # swap
+    return y
+
+
+# Load the results of the Random Forest by User Demographics data
+def rf1(suffix='C2'):
+    filename='../model/rf1_y_{}_hat.pkl'.format(suffix)
+    with open(filename, 'rb') as f:
+        y=pickle.load(f)
+    return y
+
+
+# Load the results of the Random Forest by Questions and Words data
+def rf2(suffix='C2'):
+    filename='../model/rf2_y_{}_hat.pkl'.format(suffix)
+    with open(filename, 'rb') as f:
+        y=pickle.load(f)
+    return y
+
+
+# Load the results of the Lasso Regression model using the full data features
+def lasso(suffix='C2'):
+    filename='../martinus/lasso_prediction_{}.pkl'.format(suffix)
+    with open(filename, 'rb') as f:
+        y=np.array(pickle.load(f))
+    return y
+
+
+# Load the results of the Linear Regression model using the full data features
+def linear_reg(suffix='C2'):
+    filename='../martinus/linear_regression_prediction_{}.pkl'.format(suffix)
+    y=np.zeros((0,2))
+    with open(filename, 'rb') as f:
+        p=pickle.load(f)
+    for pre,exp in p:
+        y=np.append(y,
+            np.append(pre[:,None],exp[:,None],axis=1),
+            axis=0)
+    return y
+
+
+# Load the results of the Ridge Regression model using the full data features
+def ridge(suffix='C2'):
+    filename='../martinus/ridge_prediction_{}.pkl'.format(suffix)
+    y=np.zeros((0,2))
+    with open(filename, 'rb') as f:
+        p=pickle.load(f)
+    for pre,exp in p:
+        y=np.append(y,
+            np.append(pre[:,None],exp[:,None],axis=1),
+            axis=0)
+    return y
+
+
+# List of methods that will be called later
+handlers=[
+    # lr_by_artist,
+    rf_by_artist,
+    rf_full,
+    gbr,
+    nn,
+    # rf1,
+    # rf2,
+    # lasso,
+    # linear_reg,
+    # ridge,
+]
+
+# Reading predicted ratings from each individual model
+full_data = []
+for handler in handlers:
+    data = handler(suffix='C_AVG')
+    full_data.append(data.tolist())
+
+X_C = []
+Y_C = np.array([item[1] for item in full_data[0]])
+
+for i in range(len(full_data[0])):
+    x = [item[i][0] for item in full_data]
+    X_C.append(x)
+
+X_C = np.array(X_C)
+print('Data loaded!')
+
+
+# Do a simple averaging on the predicted ratings from each individual model
+Y_pred = [np.mean(x) for i, x in enumerate(X_C)]
+print(mean_squared_error(Y_pred, Y_C), math.sqrt(mean_squared_error(Y_pred, Y_C)))
